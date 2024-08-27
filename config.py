d30e790c9182bc3f0f07f707fb59f3927bb4f5c3
hi-space <hispace.j@gmail.com>
2024-08-27 15:00:05 +0900

    set config with secret manager

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import boto3
+import json
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
@@ -14,4 +16,33 @@ class Settings(BaseSettings):
     S3_BUCKET: str
     DYNAMODB_TABLE: str
 
-config = Settings()
+
+def get_secrets():
+    settings = Settings()
+
+    try:
+        session = boto3.session.Session()
+        client = session.client(
+            service_name='secretsmanager',
+            region_name="us-west-2"
+        )
+        get_secret_value_response = client.get_secret_value(
+            SecretId="image-gen"
+        )
+
+        secrets = json.loads(get_secret_value_response['SecretString'])
+        settings.BEDROCK_REGION = secrets.get('BEDROCK_REGION')
+        settings.AWS_ACCESS_KEY = secrets.get('AWS_ACCESS_KEY')
+        settings.AWS_SECRET_KEY = secrets.get('AWS_SECRET_KEY')
+        settings.LLM_MODEL_ID = secrets.get('LLM_MODEL_ID')
+        settings.IMAGE_GEN_MODEL_ID = secrets.get('IMAGE_GEN_MODEL_ID')
+        settings.CDN_URL = secrets.get('CDN_URL')
+        settings.S3_BUCKET = secrets.get('S3_BUCKET')
+        settings.DYNAMODB_TABLE = secrets.get('DYNAMODB_TABLE')
+    except Exception as e:
+        return dict()
+    
+    return settings
+
+
+config = get_secrets()
